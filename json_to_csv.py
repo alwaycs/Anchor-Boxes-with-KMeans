dd138c078133297180f60696a1b6ccba85c096db
alwaysT <alwaysT2018@foxmail.com>
2019-04-09 18:01:27 +0800

    json_to_csv

---
+++ json_to_csv.py
@@ -0,0 +1,51 @@
+import os
+import glob
+import pandas as pd
+import json
+from PIL import Image
+
+
+def json_to_csv(name_list, img_dir, gt_dir):
+    json_list = []
+    for name in name_list:
+        image_path = os.path.join(img_dir, name)
+        im = Image.open(image_path)
+        width, height = im.size
+        gt_name = name.repalce('.jpg', '.json')
+        gt_path = os.path.join(gt_dir, gt_name)
+        with open (gt_path) as f:
+            anno = json.load(f)
+            lines = anno['lines']
+            for line in lines:
+                poly = line['points']
+                x1, y1, x2, y2, x3, y3, x4, y4 = poly
+                xmin = min(x1,x2,x3,x4)
+                ymin = min(y1,y2,y3,y4)
+                xmax = max(x1,x2,x3,x4)
+                ymax = max(y1,y2,y3,y4)
+                value = (name, width, height, xmin, ymin, xmax, ymax)
+                json_list.append(value)
+    column_name = ['filename', 'width', 'height', 'xmin', 'ymin', 'xmax', 'ymax']
+    json_df = pd.DataFrame(json_list, columns=column_name)
+    return json_df
+
+
+def main():
+    img_dir = "/workspace/meijinpeng/dataset/ReCTS/img/"
+    gt_dir = "/workspace/meijinpeng/dataset/ReCTS/gt/"
+    train_txt = "/workspace/meijinpeng/dataset/ReCTS/train.txt"
+    test_txt = "/workspace/meijinpeng/dataset/ReCTS/test.txt"
+    work_dir = os.getcwd()
+    with open(train_txt) as f_train:
+        train_name=f_train.readlines()
+    with open(train_txt) as f_test:
+        test_name=f_test.readlines()
+    all_name = train_name.extend(test_name)
+
+    for flag, path_list in zip(['train', 'test', 'all'], [train_name, test_name, all_name]):
+        xml_df = json_to_csv(path_list, img_dir, gt_dir)
+        xml_df.to_csv((work_dir + flag + '_labels.csv'), index=None)
+        print('Successfully converted json to csv.')
+
+
+main()
