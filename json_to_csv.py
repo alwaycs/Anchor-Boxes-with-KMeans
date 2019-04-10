6f41acef9c9558e2de0bc5014e84f476d3014c1a
alwaysT <alwaysT2018@foxmail.com>
2019-04-10 12:19:07 +0800

    Update json_to_csv.py

--- json_to_csv.py
+++ json_to_csv.py
@@ -36,18 +36,21 @@ def main():
     train_txt = "/workspace/meijinpeng/dataset/ReCTS/train.txt"
     test_txt = "/workspace/meijinpeng/dataset/ReCTS/test.txt"
     work_dir = os.getcwd()
-    print(work_dir)
     with open(train_txt) as f_train:
         train_name=f_train.readlines()
+        print(len(train_name))
     with open(train_txt) as f_test:
         test_name=f_test.readlines()
+        print(len(test_name))
     all_name = train_name.extend(test_name)
+    print(all_name, len(all_name))
 
     for flag, path_list in zip(['train', 'test', 'all'], [train_name, test_name, all_name]):
+        print('start aonvert:', flag)
         json_df = json_to_csv(path_list, img_dir, gt_dir)
         csv_path = os.path.join(work_dir, flag + '_labels.csv')
         json_df.to_csv(csv_path, index=None)
-        print('Successfully converted json to csv.')
+        print('Successfully converted ', flag, 'json to csv.')
 
 
 main()
